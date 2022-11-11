f14e8915ddb79059013d333079154c9d1e11525e
Ari Mauri <ari.mauri.jr@gmail.com>
2022-11-11 20:11:08 -0300

    Desafio conćluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+ Console.WriteLine("Smartphone Nokia:");
+ Smartphone nokia = new Nokia("123-456","Modelo 1","111111111",64);
+ nokia.Ligar();
+ nokia.InstalarAplicativo("whatsapp");
+
+ Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iphone:");
+ Smartphone iphone = new Iphone("456-789","Modelo 2","222222222",128);
+ iphone.ReceberLigacao();
+ iphone.InstalarAplicativo("Telegran");
